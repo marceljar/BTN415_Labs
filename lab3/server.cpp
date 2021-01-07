b6ec7a1938f0a4789db840e31bfb797abade28d0
Marcel Jar <marcel.jar@senecacollege.ca>
2021-01-07 14:51:54 -0500

    Create server.cpp

---
+++ lab3/server.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <windows.networking.sockets.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+int main(){
+    
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cout << "Could not start DLLs" << std::endl;
+        return 0;
+    }
+
+    SOCKET ListenSocket;
+    ListenSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ListenSocket == INVALID_SOCKET) {
+        std::cout << "Could not create socket" << std::endl;
+        WSACleanup();
+        return 0;
+    }
+
+    struct sockaddr_in SvrAddr;
+    SvrAddr.sin_family = AF_INET;
+    SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    SvrAddr.sin_port = htons(27000);
+    if (bind(ListenSocket, (struct sockaddr*)&SvrAddr, 
+        sizeof(SvrAddr)) == SOCKET_ERROR) {
+        std::cout << "Could not bind socket to address" << std::endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 0;
+    }
+
+    if (listen(ListenSocket, 1) == SOCKET_ERROR) {
+        std::cout << "Could not start to listen" << std::endl;
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 0;
+    }
+
+    std::cout << "Waiting for client connection" << std::endl;
+
+    SOCKET ClientSocket;
+    ClientSocket = accept(ListenSocket, NULL, NULL);
+    if (ClientSocket == INVALID_SOCKET) {
+        std::cout << "Failed to accept connection" << std::endl; 
+        closesocket(ListenSocket);
+        WSACleanup();
+        return 0;
+    }
