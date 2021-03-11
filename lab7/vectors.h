896b6cc2fff591084194273ecd08992de30c1d41
Marcel Jar <marcel.jar@senecacollege.ca>
2021-03-11 15:52:24 -0500

    Create vectors.h

---
+++ lab7/vectors.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+class Vector;
+
+class List {
+private:
+	Vector* head;
+	Vector* current;
+public:
+	List();
+	void add_vector(std::string);
+	bool remove_vector(std::string);
+	bool append_vector(std::string);
+	bool print_vector(std::string);
+	void print_vectors();
+};
+
+class Vector {
+private:
+	std::string name;
+	Vector* next;
+	int size;
+	int* elements;
+public:
+	Vector();
+	Vector(std::string);
+	~Vector();
+	friend void List::add_vector(std::string);
+	friend bool List::remove_vector(std::string);
+	friend bool List::append_vector(std::string);
+	friend bool List::print_vector(std::string);
+	friend void List::print_vectors();
+};
+
