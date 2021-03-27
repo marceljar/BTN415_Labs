00c0bad02265e11b65c4e3d194559a0647b5bacb
Marcel Jar <marcel.jar@senecacollege.ca>
2021-03-26 21:23:59 -0400

    Create main.cpp

---
+++ lab9/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+
+struct packet {
+    char number;
+    unsigned char urg : 1;
+    unsigned char ack : 1;
+    unsigned char psh : 1;
+    unsigned char rst : 1;
+    unsigned char syn : 1;
+    unsigned char fin : 1;
+    unsigned char : 2; //padding
+    unsigned int size;
+    char* data;
+    unsigned char crc;
+    unsigned int checksum;
+};
+
+void display(unsigned char byte) {
+    for (int k = 0; k < 8; k++) {
+        std::cout << (int)(byte >> 7);
+        byte = byte << 1;
+    }
+    std::cout << std::endl;
+}
+
+unsigned char calculate_packet_crc(packet my_packet) {
+    //write your code here
+}
+
+unsigned int calculate_packet_checksum(packet my_packet) {
+    //write your code here
+}
+
+int main() {
+
+    packet my_packet;
+
+    my_packet.number = 1;
+    my_packet.urg = 0;
+    my_packet.ack = 1;
+    my_packet.psh = 0;
+    my_packet.rst = 1;
+    my_packet.syn = 0;
+    my_packet.fin = 1;
+    std::cout << "Enter your message " << std::endl;
+    std::string DataBuffer;
+    std::getline(std::cin, DataBuffer);
+    my_packet.data = new char[DataBuffer.length()];
+    strcpy(my_packet.data, DataBuffer.c_str());
+    my_packet.crc = calculate_packet_crc(my_packet);
+    my_packet.checksum = calculate_packet_checksum(my_packet);
+    
+    return 0;
+}
