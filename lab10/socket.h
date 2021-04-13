62e46445f333a8831e1ad2853a14a17fe9c99bb0
Marcel Jar <marcel.jar@senecacollege.ca>
2021-04-12 21:34:49 -0400

    Update socket.h

--- lab10/socket.h
+++ lab10/socket.h
@@ -1,1 +1,102 @@
-//to be added after lab 8
+#pragma once
+#include <string>
+#include <iostream>
+#include <windows.networking.sockets.h>
+
+#define MAX_BUFFER_SIZE 512
+
+struct new_packet {
+    char number;
+    unsigned char urg : 1;
+    unsigned char ack : 1;
+    unsigned char psh : 1;
+    unsigned char rst : 1;
+    unsigned char syn : 1;
+    unsigned char fin : 1;
+    unsigned char : 2; //padding
+    unsigned int size;
+    char* data;
+    unsigned char crc;
+    unsigned int checksum;
+};
+
+void display(unsigned char byte);
+unsigned char calculate_new_packet_crc(new_packet my_packet);
+unsigned int calculate_new_packet_checksum(new_packet my_packet);
+
+struct packet {
+    char name[16];
+    bool flag;
+    int size;
+    char* letters;
+};
+
+struct serialized_packet {
+    char* data;
+    int length;
+};
+
+packet create_packet();
+serialized_packet packet_serializer(packet);
+packet packet_deserializer(char*);
+
+class Node {
+public:
+    static bool dlls_started;
+    static int num_nodes;
+    std::string ip;
+    std::string role;
+    std::string protocol;
+    int port;
+    SOCKET active_socket;
+public:
+    Node();
+    Node(std::string ip, int port);
+    ~Node();
+    void start_dlls() const;
+    bool create_socket();
+    void display_info() const;
+};
+
+class Server_TCP : public Node {
+private:
+    SOCKET client_socket;
+public:
+    Server_TCP();
+    Server_TCP(std::string ip, int port);
+    ~Server_TCP();
+    bool bind_socket();
+    bool listen_for_connections();
+    bool accept_connection();
+    int send_message(std::string message);
+    int send_packet(packet message);
+    int receive_message(std::string& message);
+    int receive_packet(packet& my_packet);
+    void close_connection();
+};
+
+class Client_TCP : public Node {
+public:
+    Client_TCP();
+    bool connect_socket(std::string ip, int port);
+    int send_message(std::string message);
+    int send_packet(packet message);
+    int receive_message(std::string& message);
+    int receive_packet(packet& my_packet);
+};
+
+class Client_UDP : public Node {
+public:
+    Client_UDP();
+    int send_message(std::string message, std::string ip, int port);
+    struct sockaddr_in receive_message(std::string& message);
+};
+
+class Server_UDP : public Node {
+public:
+    Server_UDP();
+    Server_UDP(std::string ip, int port);
+    bool bind_socket();
+    int send_message(std::string message, std::string ip, int port);
+    struct sockaddr_in receive_message(std::string& message);
+};
