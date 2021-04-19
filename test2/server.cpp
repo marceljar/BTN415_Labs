6e921186d4901d9b11c174ce9e54139ef2c0b72f
Marcel Jar <marcel.jar@senecacollege.ca>
2021-04-18 21:37:13 -0400

    Create server.cpp

---
+++ test2/server.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "socket.h"
+
+int main() {
+	data_packet recovered_packet;
+
+	Server_TCP server("127.0.0.1", 27000);
+
+	server.create_socket();
+	server.bind_socket();
+	server.listen_for_connections();
+	std::cout << "Waiting for a client to connect." << std::endl;
+	server.accept_connection();
+
+	server.receive_data_packet(recovered_packet);
+	std::cout << "data: " ;
+	for (int i = 0; i < recovered_packet.length; i++) {
+		std::cout << recovered_packet.data[i];
+	}
+	std::cout << std::endl;
+	server.send_message("Got your message!");
+
+	return 0;
+}
